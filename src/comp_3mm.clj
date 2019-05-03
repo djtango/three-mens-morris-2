2343ebffe8ae308c3b0e0c7e8c3d417452cde446
Deon Tan <deonjtan@gmail.com>
2019-05-03 13:19:28 +0100

    init

---
+++ src/comp_3mm.clj
@@ -0,0 +1,167 @@
+(ns comp-3mm
+  (:require [clojure.spec.alpha :as s]))
+
+(s/def ::position (into #{} (range 9)))
+
+(defn add-move [moves next-move]
+  (conj moves next-move))
+
+(defn current-player [moves]
+  (let [white-or-black (if (odd? (count moves))
+                         even? ;; white
+                         odd? ;; black
+                         )]
+    (->> moves
+         (map-indexed (fn [idx itm]
+                        (if (white-or-black idx)
+                          itm
+                          nil)))
+         (filter identity))))
+
+(defn latest-positions [moves]
+  (reduce (fn [state [from to]]
+            (-> state
+                (conj to)
+                (disj from)))
+          #{}
+          moves))
+
+(def horizontal-line?
+  #{#{0 1 2}
+    #{3 4 5}
+    #{6 7 8}})
+
+(defn horizontal? [moves]
+  (horizontal-line? (latest-positions moves)))
+
+(def vertical-line?
+  #{#{0 3 6}
+    #{1 4 7}
+    #{2 5 8}})
+
+(defn vertical? [moves]
+  (vertical-line? (latest-positions moves)))
+
+(def diagonal-line?
+  #{#{0 4 8}
+    #{2 4 6}})
+
+(defn diagonal? [moves]
+  (diagonal-line? (latest-positions moves)))
+
+(defn any-three-in-a-line? [moves]
+  (or (horizontal? moves)
+      (vertical? moves)
+      (diagonal? moves)))
+
+(defn win? [moves]
+  (->> moves
+       current-player
+       any-three-in-a-line?))
+
+(defn parse-input [input]
+  (try (Integer/parseInt input)
+       (catch Exception _ ::parse-input.bad-input)))
+
+(defn adjacent? [from to]
+  (let [right inc
+        left dec
+        down (partial + 3)
+        up (partial + -3) ;; a-b != b-a
+        =to (partial = to)
+        left-edge #{0 3 6}
+        right-edge #{2 5 8}]
+    (or (-> from up =to)
+        (-> from down =to)
+        (-> from right =to
+            (and (not (right-edge from))))
+        (-> from left =to
+            (and (not (left-edge from))))
+        (-> from up right =to
+            (and (not (right-edge from))))
+        (-> from down right =to
+            (and (not (right-edge from))))
+        (-> from up left =to
+            (and (not (left-edge from))))
+        (-> from down left =to
+            (and (not (left-edge from)))))))
+
+(defn occupied? [state posn]
+  (contains? (latest-positions state) posn))
+
+(defn allowed? [state from to]
+  (and (occupied? state from)
+       (not (occupied? state to))
+       (adjacent? from to)))
+
+(defn get-player [selector state]
+  (->> state
+       (map-indexed vector)
+       (filter (comp selector first))
+       (map second)))
+
+(def white-player (partial get-player odd?))
+(def black-player (partial get-player even?))
+
+(defn set-pieces [state k current-board]
+  (let [select-player (if (= k :white)
+                        white-player
+                        black-player)]
+    (->> state
+         select-player
+         latest-positions
+         (reduce (fn [board posn]
+                   (assoc board posn k))
+                 current-board))))
+
+(defn get-board [state]
+  (let [empty-board (into [] (repeat 9 nil))]
+    (->> empty-board
+         (set-pieces state :white)
+         (set-pieces state :black))))
+
+(defn print-board [state]
+  (->> state
+       get-board
+       (map {nil " "
+             :black "x"
+             :white "o"})
+       (map (fn [x]
+              (str " " x " ")))
+       (partition 3)
+       (map (partial clojure.string/join "|"))
+       (interpose (clojure.string/join "" (repeat 11 "-")))
+       (run! println)))
+
+(defn get-move [state]
+  (if (< (count state) 6)
+    (do (println "enter a position 0-9 :")
+        (let [input (read-line)
+              i (parse-input input)]
+          (if (s/valid? ::position i)
+            [nil i]
+            (get-move state))))
+
+    (do (let [piece-to-move (atom nil)
+              new-position (atom nil)]
+          (println "choose a piece to move:")
+          (reset! piece-to-move (parse-input (read-line)))
+          (println "choose where to move the piece:")
+          (reset! new-position (parse-input (read-line)))
+          (if (and (s/valid? ::position @piece-to-move)
+                   (s/valid? ::position @new-position)
+                   (allowed? @piece-to-move @new-position))
+            [@piece-to-move @new-position]
+            (get-move state))))))
+
+(defn play []
+  (println "welcome to three mens moris")
+  (loop [state [[]]]
+    (println "current board:")
+    (print-board state)
+    (let [next-move (get-move state)
+          next-state (add-move state next-move)]
+      (if (win? next-state)
+        (do (print-board state)
+            (println "game over"))
+        (recur next-state)))))
---
+++ test/comp_3mm_test.clj
@@ -0,0 +1,107 @@
+(ns comp-3mm-test
+  (:require [clojure.test :refer :all]
+            [comp-3mm :as sut]))
+
+(deftest empty-state
+  (testing "empty state should not be a win"
+    (let [empty-state [[]]]
+     (is (not (sut/win? empty-state))))))
+
+(deftest first-move
+  (testing "first move should not be a win"
+    (let [first-move [[nil 0]]]
+      (is (not (sut/win? first-move))))))
+
+(deftest line-alternating
+  (testing "a line of alternating moves should not be a win"
+    (let [w-top-left [nil 0]
+          b-top-mid [nil 1]
+          w-top-right [nil 2]
+          moves [w-top-left
+                 b-top-mid
+                 w-top-right]]
+      (is (not (sut/win? moves))))))
+
+(deftest line
+  (testing "a white line should be a win"
+    (let [w-top-left [nil 0]
+          b-mid-left [nil 3]
+          w-top-mid [nil 1]
+          b-mid-mid [nil 4]
+          w-top-right [nil 2]
+          moves [w-top-left
+                 b-mid-left
+                 w-top-mid
+                 b-mid-mid
+                 w-top-right]]
+      (is (sut/win? moves)))))
+
+(deftest adjacent?
+  ;; 0 1 2
+  ;; 3 4 5
+  ;; 6 7 8
+  (are [from to] (sut/adjacent? from to)
+       ;; horizontal
+       0 1, 1 0
+       1 2, 2 1
+
+       3 4, 4 3
+       4 5, 5 4
+
+       6 7, 7 6
+       7 8, 8 7
+       ;; vertical
+       0 3, 3 0
+       3 6, 6 3
+
+       1 4, 4 1
+       4 7, 7 4
+
+       2 5, 5 2
+       5 8, 8 5
+       ;; diagonal
+       0 4, 4 0
+       1 5, 5 1
+
+       3 7, 7 3
+       4 8, 8 4
+
+       1 3, 3 1
+       2 4, 4 2
+
+       4 6, 6 4
+       5 7, 7 5)
+
+  (are [from to] (not (sut/adjacent? from to))
+       ;; horizontal
+       2 3, 3 2
+       5 6, 6 5
+       ;; diagonal
+       2 6, 6 2
+       3 5, 5 3
+       2 0, 0 2
+       8 6, 8 6)
+  ;; collisions
+  (are [from to] (not (sut/adjacent? from to))
+       2 2
+       ))
+
+(deftest allowed?
+  (let [first-six-moves [[nil 0] [nil 1] [nil 2]
+                         [nil 3] [nil 4] [nil 5]]]
+    (testing "adjacent move is allowed"
+      (is (sut/allowed? first-six-moves [0 1])))
+    (testing "should not be able to move something to itself"
+      (is (not (sut/allowed? first-six-moves [0 0]))))
+    (testing "should not be able to move something to an occupied spot")
+    (testing "should not be able to move a piece you don't own")
+    (testing "should not be able to move an unoccupied piece")))
+
+(deftest get-board
+  (let [first-six-moves [[nil 0] [nil 1] [nil 2]
+                         [nil 3] [nil 4] [nil 5]]]
+    (is (= [:white :black :white :black :white :black nil nil nil]
+           (sut/get-board first-six-moves)))
+    (is (= [nil :black :white :black :white :black nil nil :white]
+           (sut/get-board (-> first-six-moves
+                              (conj [0 8])))))))

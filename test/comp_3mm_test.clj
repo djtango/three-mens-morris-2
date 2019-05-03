91836e678016ba9995f4201929efd1fa5c98b757
Deon Tan <deonjtan@gmail.com>
2019-05-03 15:56:39 +0100

    cleanup

--- test/comp_3mm_test.clj
+++ test/comp_3mm_test.clj
@@ -40,51 +40,68 @@
   ;; 0 1 2
   ;; 3 4 5
   ;; 6 7 8
-  (are [from to] (sut/adjacent? from to)
-       ;; horizontal
-       0 1, 1 0
-       1 2, 2 1
+  (testing "horizontal cells should be adjacent"
+   (are [from to] (sut/adjacent? from to)
+        ;; horizontal
+        0 1, 1 0
+        1 2, 2 1
 
-       3 4, 4 3
-       4 5, 5 4
+        3 4, 4 3
+        4 5, 5 4
 
-       6 7, 7 6
-       7 8, 8 7
-       ;; vertical
-       0 3, 3 0
-       3 6, 6 3
+        6 7, 7 6
+        7 8, 8 7))
+  (testing "vertical cells should be adjacent"
+    (are [from to] (sut/adjacent? from to)
+        0 3, 3 0
+        3 6, 6 3
 
-       1 4, 4 1
-       4 7, 7 4
+        1 4, 4 1
+        4 7, 7 4
 
-       2 5, 5 2
-       5 8, 8 5
-       ;; diagonal
-       0 4, 4 0
-       1 5, 5 1
+        2 5, 5 2
+        5 8, 8 5
+        ;; diagonal
+        0 4, 4 0
+        1 5, 5 1
 
-       3 7, 7 3
-       4 8, 8 4
+        3 7, 7 3
+        4 8, 8 4
 
-       1 3, 3 1
-       2 4, 4 2
+        1 3, 3 1
+        2 4, 4 2
 
-       4 6, 6 4
-       5 7, 7 5)
+        4 6, 6 4
+        5 7, 7 5))
+  (testing "diagonal cells should be adjacent"
+    (are [from to] (sut/adjacent? from to)
+         0 4, 4 0
+         1 5, 5 1
 
-  (are [from to] (not (sut/adjacent? from to))
-       ;; horizontal
-       2 3, 3 2
-       5 6, 6 5
-       ;; diagonal
-       2 6, 6 2
-       3 5, 5 3
-       2 0, 0 2
-       8 6, 8 6)
+         3 7, 7 3
+         4 8, 8 4
+
+         1 3, 3 1
+         2 4, 4 2
+
+         4 6, 6 4
+         5 7, 7 5))
+  (testing "row overflow should not be adjacent"
+    (are [from to] (not (sut/adjacent? from to))
+         ;; horizontal
+         2 3, 3 2
+         5 6, 6 5
+         ;; diagonal
+         2 6, 6 2
+         3 5, 5 3
+         2 0, 0 2
+         8 6, 8 6))
   ;; collisions
-  (are [from to] (not (sut/adjacent? from to))
-       2 2
-       ))
+  (testing "the same cell should not be adjacent"
+    (are [cell] (not (sut/adjacent? cell cell))
+         0 1 2
+         3 4 5
+         6 7 8)))
 
 (deftest allowed?
   (let [first-six-moves [[nil 0] [nil 1] [nil 2]
